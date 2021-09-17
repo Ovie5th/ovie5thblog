367d4659c814162e63e6f400bb84f2b381e612d3
Ovie5th <Ovie.idholo@decagon.dev>
2021-09-17 01:40:47 +0100

    topbar done

---
+++ src/topbar/TopBar.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import "./TopBar.css"
+
+export default function TopBar() {
+    return (
+        <div className = "top">
+            <div className="topleft">
+            <i className="topIcon fab fa-twitter"></i>
+            <i className="topIcon fab fa-instagram"></i>
+            <i className="topIcon fab fa-linkedin"></i>
+            </div>
+            <div className="topcenter">
+                <ul className = "toplist" > 
+                    <li className = "toplistItem">HOME</li>
+                    <li className = "toplistItem">ABOUT</li>
+                    <li className = "toplistItem">CONTACT</li>
+                    <li className = "toplistItem">WRITE</li>
+                    <li className = "toplistItem">LOGOUT</li>
+                </ul>
+            </div>
+            <div className="topright">
+                <img className = "topImg" src="" alt="" />
+                <i class="fas fa-search"></i>
+            </div>
+        </div>
+    )
+}
