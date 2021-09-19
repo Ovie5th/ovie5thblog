b23a584e23f5b234dffae84826904356b9f217da
Ovie5th <Ovie.idholo@decagon.dev>
2021-09-19 02:19:37 +0100

    static posts added

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,14 +1,15 @@
 import "./home.css"
 import Header from "../../header/Header"
-import Post from "../../post/Post";
 import Sidebar from "../../sidebar/Sidebar";
+import Posts from "../../posts/Posts";
+
 
 export default function Home() {
     return (
         <>
         <Header/>
         <div className = "home">
-            <Post/>
+            <Posts/>
             <Sidebar/>
         </div>
         </>
--- src/post/Post.jsx
+++ src/post/Post.jsx
@@ -1,9 +1,30 @@
-import "./post.css"
+import "./post.css";
 
 export default function Post() {
     return (
-        <div className = "post"> 
-            post
+        <div className="post">
+            <div className="postImgdiv">
+                <img className="postImg" src="images/anime.jpg" alt="" />
+            </div>
+
+            <div className="postInfo">
+                <div className="postCategories">
+                    <span className="postCategory">Anime</span>
+                    <span className="postCategory">Movie</span>
+                </div>
+                <span className="postTitle">Hello World</span>
+                <hr />
+                <span className="postDate">1 Hour Ago</span>
+                <div className="postDesc">
+                    <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque 
+                        laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi 
+                        architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia 
+                        voluptas sit aspernatur aut odit aut fugit,audantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi 
+                        architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia 
+                        voluptas sit aspernatur aut odit aut fugit, </p>
+                </div>
+            </div>
+
         </div>
-    )
+    );
 }
